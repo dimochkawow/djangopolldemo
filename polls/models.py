b775add257f171cdc49e8da5e66ec0a245cdbf1b
Dmytro Torianik <Dmytro_Torianik@epam.com>
2019-07-03 13:58:33 +0300

    Django tutorial - Part 4

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -13,6 +12,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
